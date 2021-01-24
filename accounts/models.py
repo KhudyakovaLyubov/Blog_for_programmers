573746c894ffddb2a95bae1100a59a844162c92e
DeveloperKhudyakova <40723252+KhudyakovaLyubov@users.noreply.github.com>
2021-01-25 02:43:58 +0400

    add User Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50, blank=True)
+    GENDER_CHOICES = (
+        ('Женский', 'Woman'),
+        ('Мужской', 'Man'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    dateOfBirth = models.DateField(blank=True)
+    description = models.CharField(max_length=250, blank=True)
+    city = models.CharField(max_length=50)
+
+
+
